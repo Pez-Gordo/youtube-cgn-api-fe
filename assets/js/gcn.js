8a183c5e705e238c7eeecf31dd093c089d6a91e6
pez <emvenegas@gmail.com>
2021-05-06 14:03:12 +0100

    adjusting video frame to iphone 6 screen width

--- assets/js/gcn.js
+++ assets/js/gcn.js
@@ -6,7 +6,23 @@ $(document).ready(function() {
         type: 'post',
         dataType: 'json',     
         success: function(result) {
-            console.log(result)
+            console.log("Result--->",result)
+
+            var id = result.videoData[0]._id
+            console.log("ID--->",id)
+            mainVid(id)
+            
+            //resultsLoop(data)
         }
     })
+
+    function mainVid(id) {
+        $("#video").html(`
+        
+        <iframe width="375" height="210" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        `)
+    }
+
+    
 })
